06a83c9bdf8bdac9adc0dfa4801edfd3ec76aa56
Trey McGarity <trey.mcgarity99@gmail.com>
2020-10-02 08:56:27 -0500

    5/8 and almost 6

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,32 @@
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
 
-# Your code here
+def decode_cipher(f):
+    file = open(f, "r")
+    contents = file.read()
 
+    cache = {}
+    word_count = 0
+
+    for character in contents:
+        if character.isalpha() != True:
+            continue
+        # if char isn't in cache add it
+        if character not in cache:
+            cache[character] = 1
+        # otherwise, increment value at key 
+        else:
+            cache[character] += 1
+
+        word_count += 1
+
+    for item in cache:
+        cache[item] = round((cache[item] / word_count) * 100, 2)
+
+    key_value = sorted(list(cache.items()), key = lambda item: item[1], reverse = True)
+
+    print(key_value) #returns list of tuples of most to least frequent characters
+    return 
+
+    
+decode_cipher("./ciphertext.txt")
